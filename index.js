5ec82fe36af0a54db28a63c3318ca91e0ae9cc0b
ciolt <cioltdev@outlook.com>
2019-02-19 17:21:03 -0500

    remove old index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const toml = require('toml')
-const fs = require('fs')
-const path = require('path')
-const tomlConfig = fs.readFileSync(path.resolve(__dirname, 'config.toml'), 'utf8')
-
-const data = toml.parse(tomlConfig)
-
-module.exports = data
