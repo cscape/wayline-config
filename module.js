78fc81e3a5ad71c79e50fc94280468f47580e1ae
ciolt <cioltdev@outlook.com>
2019-04-15 02:34:16 -0400

    rebuild

--- module.js
+++ module.js
@@ -31,153 +31,174 @@ module.exports = {
         "85863": "19935",
         "85864": "19935"
       },
-      "headsigns": [
-        {
-          "40756": "North Beach",
-          "72486": "Middle Beach",
-          "73772": "Collins Link",
-          "85862": "SOBE Loop A",
-          "85863": "SOBE Loop B",
-          "85864": "SOBE Loop via 11th St"
-        }
-      ]
+      "headsigns": {
+        "40756": "North Beach",
+        "72486": "Middle Beach",
+        "73772": "Collins Link",
+        "85862": "SOBE Loop A",
+        "85863": "SOBE Loop B",
+        "85864": "SOBE Loop via 11th St"
+      }
     },
     {
       "name": "Miami",
+      "alias": "Miami Trolley",
       "id": 21241,
-      "routeMap": [
-        {
-          "71269": "16889",
-          "71276": "16885",
-          "71304": "16886",
-          "71322": "16887",
-          "71342": "16891",
-          "71344": "16890",
-          "71345": "16892",
-          "101446": "16885"
-        }
-      ],
-      "headsigns": [
-        {
-          "71269": "Allapattah",
-          "71276": "Biscayne",
-          "71304": "Brickell",
-          "71322": "Coral Way",
-          "71342": "Health District",
-          "71344": "Overtown",
-          "71345": "Stadium",
-          "75187": "Little Havana",
-          "88467": "Little Haiti",
-          "94590": "Flagami",
-          "95029": "Coconut Grove",
-          "97325": "Cross Bay Express",
-          "101446": "Biscayne (Detour)"
-        }
-      ]
+      "routeMap": {
+        "71269": "16889",
+        "71276": "16885",
+        "71304": "16886",
+        "71322": "16887",
+        "71342": "16891",
+        "71344": "16890",
+        "71345": "16892",
+        "101446": "16885"
+      },
+      "headsigns": {
+        "71269": "Allapattah",
+        "71276": "Biscayne",
+        "71304": "Brickell",
+        "71322": "Coral Way",
+        "71342": "Health District",
+        "71344": "Overtown",
+        "71345": "Stadium",
+        "75187": "Little Havana",
+        "88467": "Little Haiti",
+        "94590": "Flagami",
+        "95029": "Coconut Grove",
+        "97325": "Cross Bay Express",
+        "101446": "Biscayne (Detour)"
+      }
     },
     {
       "name": "Doral",
+      "alias": "Doral Trolley",
       "id": 2500,
-      "routeMap": [
-        {
-          "35929": "16895",
-          "95835": "16896",
-          "95836": "16897"
-        }
-      ],
-      "headsigns": [
-        {
-          "35929": "Route 1",
-          "95835": "Route 2",
-          "95836": "Route 3"
-        }
-      ]
+      "routeMap": {
+        "35929": "16895",
+        "95835": "16896",
+        "95836": "16897"
+      },
+      "headsigns": {
+        "35929": "Route 1",
+        "95835": "Route 2",
+        "95836": "Route 3"
+      }
     },
     {
       "name": "North Miami",
+      "alias": "NOMI Express",
       "id": 24600,
-      "routeMap": [
-        {
-          "91340": "17792"
-        }
-      ],
-      "headsigns": [
-        {
-          "91281": "Green",
-          "91301": "Blue",
-          "91340": "Orange",
-          "91398": "Red"
-        }
-      ]
+      "routeMap": {
+        "91340": "17792"
+      },
+      "headsigns": {
+        "91281": "Green",
+        "91301": "Blue",
+        "91340": "Orange",
+        "91398": "Red"
+      }
     },
     {
       "name": "Aventura",
+      "alias": "Aventura Express Shuttle Bus",
       "id": 26082,
-      "routeMap": [
-        {
-          
-        }
-      ],
-      "headsigns": [
-        {
-          
-        }
-      ]
+      "routeMap": {
+        
+      },
+      "headsigns": {
+        
+      }
     },
     {
       "name": "Sunny Isles Beach",
+      "alias": "Sunny Isles Beach Shuttle",
       "id": 31344,
-      "routeMap": [
-        {
-          
-        }
-      ],
-      "headsigns": [
-        {
-          
-        }
-      ]
+      "routeMap": {
+        
+      },
+      "headsigns": {
+        
+      }
     },
     {
       "name": "North Miami Beach",
+      "alias": "NMB Line",
       "id": 31934,
-      "routeMap": [
-        {
-          "76332": "18112"
-        }
-      ],
-      "headsigns": [
-        {
-          "76332": "Route A",
-          "76334": "Route B",
-          "76338": "Route C"
-        }
-      ]
+      "routeMap": {
+        "76332": "18112"
+      },
+      "headsigns": {
+        "76332": "Route A",
+        "76334": "Route B",
+        "76338": "Route C"
+      }
     },
     {
       "name": "Pinecrest",
       "alias": "Pinecrest People Mover",
-      "id": 24560
-    },
-    {
-      "name": "Miami Lakes",
-      "id": 21324
+      "id": 24560,
+      "routeMap": {
+        
+      },
+      "headsigns": {
+        
+      }
     },
     {
       "name": "Bal Harbour",
-      "id": 34323
+      "alias": "Bal Harbour Express Shuttle",
+      "id": 34323,
+      "routeMap": {
+        "69516": "17796"
+      },
+      "headsigns": {
+        "69516": "Bal Harbour"
+      }
     },
     {
       "name": "Palmetto Bay",
-      "id": 49454
+      "alias": "Village of Palmetto Bay IBUS",
+      "id": 49454,
+      "routeMap": {
+        "95974": "20408"
+      },
+      "headsigns": {
+        
+      }
     },
     {
       "name": "South Miami",
-      "id": 34423
+      "id": 34423,
+      "routeMap": {
+        
+      },
+      "headsigns": {
+        
+      }
     },
     {
       "name": "Miami-Dade Transit",
-      "id": 30109
+      "alias": "Circulator",
+      "id": 30109,
+      "routeMap": {
+        "81711": "20900",
+        "82479": "20819",
+        "83294": "20894",
+        "83299": "20895",
+        "83348": "20902",
+        "92084": "20893",
+        "92086": "20851",
+        "92092": "20854",
+        "94171": "20884"
+      },
+      "headsigns": {
+        "81711": "Brownsville",
+        "83294": "Overtown Circulator",
+        "83299": "Sweetwater Trolley",
+        "92084": "Skylake Circulator",
+        "94171": "Biscayne Gardens Circulator"
+      }
     }
   ]
 }
