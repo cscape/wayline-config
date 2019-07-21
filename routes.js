d09ab179f5143614fe4621c3d5c189b667ba7717
ciolt <cioltdev@outlook.com>
2019-07-20 20:49:00 -0400

    Update routes.js since MDT changed most routeIDs

--- routes.js
+++ routes.js
@@ -4,492 +4,492 @@
 module.exports = {
   "MiamiDadeTransit": [
     {
-      "route_id": "20819",
+      "route_id": "21358",
       "route_shortname": "1",
       "route_longname": "SO.MIAMI HTS-PERRINE VIA SOUTHLAND"
     },
     {
-      "route_id": "20820",
+      "route_id": "21387",
       "route_shortname": "2",
       "route_longname": "DOWNTOWN-163 ST MALL VIA NW 2 AVE"
     },
     {
-      "route_id": "20821",
+      "route_id": "21416",
       "route_shortname": "3",
       "route_longname": "AVENTURA-DOWNTOWN VIA BISC.BLVD."
     },
     {
-      "route_id": "20822",
+      "route_id": "21440",
       "route_shortname": "7",
       "route_longname": "DOWNTOWN-DOLPH MALL/AIRPORT VIA 7ST"
     },
     {
-      "route_id": "20823",
+      "route_id": "21447",
       "route_shortname": "8",
       "route_longname": "BRICKELL-107AV/WSTCHSTR VIA SW 8 ST"
     },
     {
-      "route_id": "20824",
+      "route_id": "21451",
       "route_shortname": "9",
       "route_longname": "AVENTURA-DOWNTOWN VIA NE 6 & 2 AVE"
     },
     {
-      "route_id": "20825",
+      "route_id": "21359",
       "route_shortname": "10",
       "route_longname": "SKYLAKE - OMNI - VIA NE12 & 2AVE"
     },
     {
-      "route_id": "20826",
+      "route_id": "21367",
       "route_shortname": "11",
       "route_longname": "FIU-DOWNTOWN VIA FLAGLER ST"
     },
     {
-      "route_id": "20827",
+      "route_id": "21373",
       "route_shortname": "12",
       "route_longname": "NORTHSIDE - MERCY HOSP."
     },
     {
-      "route_id": "20828",
+      "route_id": "21381",
       "route_shortname": "16",
       "route_longname": "163 ST-OMNI VIA 16AV & BISC. BLVD"
     },
     {
-      "route_id": "20829",
+      "route_id": "21382",
       "route_shortname": "17",
       "route_longname": "NORWOOD - VIZCAYA VIA NW-SW17AVE"
     },
     {
-      "route_id": "20830",
+      "route_id": "21384",
       "route_shortname": "19",
       "route_longname": "163 ST MALL-MDC NORTH"
     },
     {
-      "route_id": "20831",
+      "route_id": "21393",
       "route_shortname": "21",
       "route_longname": "NORTHSIDE - DOWNTOWN"
     },
     {
-      "route_id": "20832",
+      "route_id": "21398",
       "route_shortname": "22",
       "route_longname": "163 ST MALL-COCO GROVE VIA 22 AVE"
     },
     {
-      "route_id": "20833",
+      "route_id": "21400",
       "route_shortname": "24",
       "route_longname": "CORAL WAY LIMITED"
     },
     {
-      "route_id": "20834",
+      "route_id": "21406",
       "route_shortname": "27",
       "route_longname": "COCO GROVE-CALDER VIA NW 27 AVE"
     },
     {
-      "route_id": "20835",
+      "route_id": "21412",
       "route_shortname": "29",
       "route_longname": "HIALEAH TRIANGLE-MIAMI LAKES"
     },
     {
-      "route_id": "20836",
+      "route_id": "21419",
       "route_shortname": "31",
       "route_longname": "BUSWAY LOCAL CUTLER BAY-DADELAND SO"
     },
     {
-      "route_id": "20837",
+      "route_id": "21420",
       "route_shortname": "32",
       "route_longname": "MIAMI GRDNS-OMNI VIA NW 32 AVE-20ST"
     },
     {
-      "route_id": "20838",
+      "route_id": "21421",
       "route_shortname": "33",
       "route_longname": "HIA GRDNS-BISC BLVD VIA 49/103 ST"
     },
     {
-      "route_id": "20839",
+      "route_id": "21423",
       "route_shortname": "34",
       "route_longname": "EXPRESS: FLA CITY TO DADELAND SO"
     },
     {
-      "route_id": "20840",
+      "route_id": "21425",
       "route_shortname": "35",
       "route_longname": "MDC KENDALL-FLA.CITY VIA CUTLER BAY"
     },
     {
-      "route_id": "20841",
+      "route_id": "21426",
       "route_shortname": "36",
       "route_longname": "DOLPHIN-DORAL-MIA.SPGS-VIA NW 36 ST"
     },
     {
-      "route_id": "20842",
+      "route_id": "21427",
       "route_shortname": "37",
       "route_longname": "HIALEAH-SO.MIAMI VIA PALM/37 AVE"
     },
     {
-      "route_id": "20843",
+      "route_id": "21428",
       "route_shortname": "38",
       "route_longname": "BUSWAY MAX: FLA CITY TO DADELAND SO"
     },
     {
-      "route_id": "20844",
+      "route_id": "21429",
       "route_shortname": "39",
       "route_longname": "EXPRESS: S.DADE GVT CTR-DADELAND SO"
     },
     {
-      "route_id": "20845",
+      "route_id": "21430",
       "route_shortname": "40",
       "route_longname": "BIRD RD/152 AVE TO DOUGLAS RD STA"
     },
     {
-      "route_id": "20846",
+      "route_id": "21431",
       "route_shortname": "42",
       "route_longname": "DGLS RD-OPALOCKA TRI-RAIL"
     },
     {
-      "route_id": "20847",
+      "route_id": "21432",
       "route_shortname": "46",
       "route_longname": "LIBERTY CITY CONNECTION"
     },
     {
-      "route_id": "20848",
+      "route_id": "21434",
       "route_shortname": "51",
       "route_longname": "FLAGLER MAX: WEST DADE TO DOWNTOWN"
     },
     {
-      "route_id": "20849",
+      "route_id": "21435",
       "route_shortname": "52",
       "route_longname": "GOULDS TO DADELAND SOUTH STATION"
     },
     {
-      "route_id": "20850",
+      "route_id": "21436",
       "route_shortname": "54",
       "route_longname": "HIALEAH GRDNS-BISC BLVD VIA 54 ST"
     },
     {
-      "route_id": "20851",
+      "route_id": "21437",
       "route_shortname": "56",
       "route_longname": "CHILDREN HOSP TO 162 AVE VIA 56 ST"
     },
     {
-      "route_id": "20852",
+      "route_id": "21438",
       "route_shortname": "57",
       "route_longname": "MIA STATION TO SW 152 ST VIA 57 AVE"
     },
     {
-      "route_id": "20853",
+      "route_id": "21439",
       "route_shortname": "62",
       "route_longname": "HIALEAH-BISC BLVD VIA 62 ST"
     },
     {
-      "route_id": "20854",
+      "route_id": "21441",
       "route_shortname": "71",
       "route_longname": "DOLPHIN MALL-MDC KEND.VIA 107/112AV"
     },
     {
-      "route_id": "20855",
+      "route_id": "21442",
       "route_shortname": "72",
       "route_longname": "SO.MIAMI-BENT TREE/162AV VIA 72 ST"
     },
     {
-      "route_id": "20856",
+      "route_id": "21443",
       "route_shortname": "73",
       "route_longname": "NW DADE-DADELAND SO. VIA 67/72 AVE"
     },
     {
-      "route_id": "20857",
+      "route_id": "21444",
       "route_shortname": "75",
       "route_longname": "MIAMI LAKES-163ST MALL-FIU BISC BAY"
     },
     {
-      "route_id": "20858",
+      "route_id": "21445",
       "route_shortname": "77",
       "route_longname": "NORWOOD - DOWNTOWN VIA NW 7 AVE"
     },
     {
-      "route_id": "20859",
+      "route_id": "21446",
       "route_shortname": "79",
       "route_longname": "79_ST MAX: MIA BCH-NORTHSIDE"
     },
     {
-      "route_id": "20860",
+      "route_id": "21448",
       "route_shortname": "82",
       "route_longname": "WESTCHESTER CIRCULATOR"
     },
     {
-      "route_id": "20861",
+      "route_id": "21449",
       "route_shortname": "87",
       "route_longname": "PALMETTO/DORAL-DADELAND VIA 87 AV"
     },
     {
-      "route_id": "20862",
+      "route_id": "21450",
       "route_shortname": "88",
       "route_longname": "162 AVE TO DADELAND NORTH VIA 88 ST"
     },
     {
-      "route_id": "20863",
+      "route_id": "21452",
       "route_shortname": "93",
       "route_longname": "BISCAYNE MAX: DOWNTOWN-AVENTURA"
     },
     {
-      "route_id": "20864",
+      "route_id": "21453",
       "route_shortname": "95",
       "route_longname": "I-95 GOLDEN GLADES EXPRESS"
     },
     {
-      "route_id": "20865",
+      "route_id": "21454",
       "route_shortname": "99",
       "route_longname": "NW DADE-AVENTURA MALL"
     },
     {
-      "route_id": "20866",
+      "route_id": "21360",
       "route_shortname": "101",
       "route_longname": "A-VIA VENETIAN CAUSEWAY"
     },
     {
-      "route_id": "20867",
+      "route_id": "21361",
       "route_shortname": "102",
       "route_longname": "B-BRICKELL STATION-KEY BISCAYNE"
     },
     {
-      "route_id": "20868",
+      "route_id": "21362",
       "route_shortname": "103",
       "route_longname": "C-MT SINAI HOSP-SOUTH POINTE"
     },
     {
-      "route_id": "20869",
+      "route_id": "21363",
       "route_shortname": "104",
       "route_longname": "HAMMOCKS-MDC-DADELAND NO.VIA 104 ST"
     },
     {
-      "route_id": "20870",
+      "route_id": "21364",
       "route_shortname": "105",
       "route_longname": "E-GOLDEN GLDS.-AVENTURA/HALLANDALE"
     },
     {
-      "route_id": "20871",
+      "route_id": "21365",
       "route_shortname": "107",
       "route_longname": "G-MDC NORTH - BEACH/96 ST"
     },
     {
-      "route_id": "20872",
+      "route_id": "21366",
       "route_shortname": "108",
       "route_longname": "H-163 ST MALL - HAULOVER"
     },
     {
-      "route_id": "20873",
+      "route_id": "21368",
       "route_shortname": "110",
       "route_longname": "J-MIAMI BCH-MIA STAT  VIA 36 ST"
     },
     {
-      "route_id": "20874",
+      "route_id": "21369",
       "route_shortname": "112",
       "route_longname": "L-HIALEAH-MIA BCH/LINC RD VIA 79 ST"
     },
     {
-      "route_id": "20875",
+      "route_id": "21370",
       "route_shortname": "113",
       "route_longname": "M-CIVIC CENTER-MIAMI BEACH"
     },
     {
-      "route_id": "20876",
+      "route_id": "21371",
       "route_shortname": "115",
       "route_longname": "MID-NORTH-BEACH CONNECTION"
     },
     {
-      "route_id": "20877",
+      "route_id": "21372",
       "route_shortname": "119",
       "route_longname": "S-AVENTURA-DOWNTOWN VIA COLLINS AVE"
     },
     {
-      "route_id": "20878",
+      "route_id": "21374",
       "route_shortname": "120",
       "route_longname": "BEACH MAX: DOWNTOWN-AVENTURA MALL"
     },
     {
-      "route_id": "20879",
+      "route_id": "21375",
       "route_shortname": "132",
       "route_longname": "HIA.TRI-RAIL STA-DORAL VIA 36ST"
     },
     {
-      "route_id": "20880",
+      "route_id": "21376",
       "route_shortname": "135",
       "route_longname": "135_STREET CROSSTOWN"
     },
     {
-      "route_id": "20881",
+      "route_id": "21377",
       "route_shortname": "136",
       "route_longname": "DOUGLAS RD-OLD CUTLER-SW 136 ST"
     },
     {
-      "route_id": "20882",
+      "route_id": "21378",
       "route_shortname": "137",
       "route_longname": "WEST DADE CONNECTION"
     },
     {
-      "route_id": "20883",
+      "route_id": "21379",
       "route_shortname": "150",
       "route_longname": "MIAMI BEACH AIRPORT FLYER"
     },
     {
-      "route_id": "20884",
+      "route_id": "21380",
       "route_shortname": "155",
       "route_longname": "BISCAYNE GARDENS CIRCULATOR"
     },
     {
-      "route_id": "20885",
+      "route_id": "21383",
       "route_shortname": "183",
-      "route_longname": "NW 67 AV/186 ST - AVENTURA"
+      "route_longname": "NW 73 AV/186 ST - AVENTURA"
     },
     {
-      "route_id": "20886",
+      "route_id": "21385",
       "route_shortname": "195",
       "route_longname": "DADE-BROWARD EXPRESS (BROWARD BLVD)"
     },
     {
-      "route_id": "20887",
+      "route_id": "21386",
       "route_shortname": "196",
       "route_longname": "DADE-BROWARD EXPRESS (SHERIDAN ST)"
     },
     {
-      "route_id": "20888",
+      "route_id": "21388",
       "route_shortname": "200",
       "route_longname": "CUTLER BAY LOCAL"
     },
     {
-      "route_id": "20889",
+      "route_id": "21389",
       "route_shortname": "202",
       "route_longname": "LITTLE HAITI CONNECTION"
     },
     {
-      "route_id": "20890",
+      "route_id": "21390",
       "route_shortname": "204",
       "route_longname": "KILLIAN KAT"
     },
     {
-      "route_id": "20891",
+      "route_id": "21391",
       "route_shortname": "207",
       "route_longname": "LITTLE HAVANA CONNECTION"
     },
     {
-      "route_id": "20892",
+      "route_id": "21392",
       "route_shortname": "208",
       "route_longname": "LITTLE HAVANA CONNECTION"
     },
     {
-      "route_id": "20893",
+      "route_id": "21394",
       "route_shortname": "210",
       "route_longname": "SKYLAKE CIRCULATOR"
     },
     {
-      "route_id": "20894",
+      "route_id": "21395",
       "route_shortname": "211",
       "route_longname": "OVERTOWN CIRCULATOR"
     },
     {
-      "route_id": "20895",
+      "route_id": "21396",
       "route_shortname": "212",
       "route_longname": "SWEETWATER CIRCULATOR"
     },
     {
-      "route_id": "20896",
+      "route_id": "21397",
       "route_shortname": "217",
       "route_longname": "BUNCHE PARK CIRCULATOR"
     },
     {
-      "route_id": "20897",
+      "route_id": "21399",
       "route_shortname": "238",
       "route_longname": "EAST-WEST CONNECTION"
     },
     {
-      "route_id": "20898",
+      "route_id": "21401",
       "route_shortname": "246",
       "route_longname": "NIGHT OWL"
     },
     {
-      "route_id": "21033",
+      "route_id": "21402",
       "route_shortname": "248",
       "route_longname": "PRINCETON CIRCULATOR"
     },
     {
-      "route_id": "20899",
+      "route_id": "21403",
       "route_shortname": "252",
       "route_longname": "CORAL REEF MAX"
     },
     {
-      "route_id": "20900",
+      "route_id": "21404",
       "route_shortname": "254",
       "route_longname": "BROWNSVILLE CIRCULATOR"
     },
     {
-      "route_id": "20901",
+      "route_id": "21405",
       "route_shortname": "267",
       "route_longname": "LUDLAM LIMITED"
     },
     {
-      "route_id": "20902",
+      "route_id": "21407",
       "route_shortname": "272",
       "route_longname": "SUNSET KAT"
     },
     {
-      "route_id": "20903",
+      "route_id": "21408",
       "route_shortname": "277",
       "route_longname": "7 AVE MAX"
     },
     {
-      "route_id": "20904",
+      "route_id": "21409",
       "route_shortname": "286",
       "route_longname": "NORTH POINTE CIRCULATOR"
     },
     {
-      "route_id": "20905",
+      "route_id": "21410",
       "route_shortname": "287",
       "route_longname": "SAGA BAY MAX"
     },
     {
-      "route_id": "20906",
+      "route_id": "21411",
       "route_shortname": "288",
       "route_longname": "KENDALL CRUISER"
     },
     {
-      "route_id": "20907",
+      "route_id": "21413",
       "route_shortname": "295",
       "route_longname": "I-95 FT LAUD-CIVIC CENTER EXPRESS"
     },
     {
-      "route_id": "20908",
+      "route_id": "21414",
       "route_shortname": "296",
       "route_longname": "I-95 SHERIDAN-CIVIC CENTER EXPRESS"
     },
     {
-      "route_id": "20909",
+      "route_id": "21415",
       "route_shortname": "297",
       "route_longname": "27 AVE ORANGE MAX"
     },
     {
-      "route_id": "20910",
+      "route_id": "21417",
       "route_shortname": "301",
       "route_longname": "DADE/MONROE EXPRESS"
     },
     {
-      "route_id": "20911",
+      "route_id": "21418",
       "route_shortname": "302",
       "route_longname": "CARD SOUND EXPRESS"
     },
     {
-      "route_id": "20912",
+      "route_id": "21422",
       "route_shortname": "338",
       "route_longname": "WEEKEND EXPRESS"
     },
     {
-      "route_id": "20913",
+      "route_id": "21424",
       "route_shortname": "344",
       "route_longname": "MDC HOMESTEAD TO FLA CITY"
     },
     {
-      "route_id": "20914",
+      "route_id": "21433",
       "route_shortname": "500",
       "route_longname": "MIDNIGHT OWL"
     },
     {
-      "route_id": "21024",
+      "route_id": "21354",
       "route_shortname": "RAIL",
       "route_longname": "REGULAR METRORAIL SCHEDULE"
     },
